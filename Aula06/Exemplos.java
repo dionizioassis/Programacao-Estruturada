6b807be9ef6cbab40dd631d2adf2ced5604e5850
dionisio300 <dionisioassis80@gmail.com>
2026-08-21 10:37:12 -0300

    Aula 06

--- Aula06/Exemplos.java
+++ Aula06/Exemplos.java
@@ -5,18 +5,28 @@ import java.util.Scanner;
 public class Exemplos {
     public static void main(String[] args) {
         //Uma loja concede 10% de desconto quando o valor da compra é de pelo menos R$ 200. O programa deve ler o valor da compra, aplicar o desconto somente quando a condição for verdadeira e mostrar o desconto e o total final.
+
+        // Scanner dados = new Scanner(System.in);
+        // System.out.println("Digite o valor da compra");
+        // double valorCompra = dados.nextDouble();
+        // double desconto = 0;
+
+        // if (valorCompra >= 200){
+        //     desconto = valorCompra * 0.1;
+        // }
+        // valorCompra = valorCompra - desconto;
+        // System.out.print("O valor a ser pago é: " + valorCompra);
+
+        // dados.close();
+
+        //um vendedor recebe comissão de 8% quando atinge R$ 10.000 ou mais em vendas no mês; caso contrário, recebe 4%. O programa lê salário-base e total de vendas, calcula a comissão e mostra o salário final.
+
         
-        Scanner dados = new Scanner(System.in);
-        System.out.println("Digite o valor da compra");
-        double valorCompra = dados.nextDouble();
-        double desconto = 0;
 
-        if (valorCompra >= 200){
-            desconto = valorCompra * 0.1;
-        }
-        valorCompra = valorCompra - desconto;
-        System.out.print("O valor a ser pago é: " + valorCompra);
 
-        dados.close();
     }
+
+    
+
+
 }
