ad31e775e577b5af02f483a48e6fd33de2cd4459
dionisio300 <dionisioassis80@gmail.com>
2026-08-28 11:05:06 -0300

    Aula 08

---
+++ Aula08/projeto.java
@@ -0,0 +1,26 @@
+public class projeto {
+    public static void main(String[] args) {
+        /*
+           Desenvolva um programa em Java que simule um sistema bancário simples. O cliente inicia com saldo de R$ 1.000,00 e o sistema deve apresentar repetidamente o seguinte menu:
+            ===== BANCO Bubank  =====
+            1 - Consultar saldo
+            2 - Realizar depósito
+            3 - Realizar saque
+            4 - Exibir extrato
+            5 - Mostrar maior depósito
+            0 - Sair
+            Escolha uma opção:
+
+            Considere as seguintes regras:
+                •	Consultar saldo: apresentar o saldo atual da conta. 
+                •	Realizar depósito: solicitar um valor positivo, adicioná-lo ao saldo e registrar a operação. 
+                •	Realizar saque: solicitar um valor positivo. O saque somente poderá ser realizado se houver saldo suficiente. Após o saque, a operação deverá ser registrada. 
+                •	Exibir extrato: apresentar todas as movimentações realizadas durante a execução do programa. 
+                •	Mostrar maior depósito: percorrer as movimentações armazenadas e apresentar o maior depósito realizado. 
+                •	Sair: encerrar o programa. 
+                •	Qualquer opção diferente das disponíveis deverá apresentar a mensagem "Opção inválida".
+
+        
+        */
+    }
+}
